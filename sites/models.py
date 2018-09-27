53f4b5ae3fc54645ea78c724b53b5aa9790ee58c
John Blischak <jdblischak@gmail.com>
2018-09-27 16:08:53 -0400

    Configure and activate initial models.

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Publication(models.Model):
+    doi = models.CharField(max_length=200)
+    pmid = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.doi
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    author = models.ManyToManyField(User)
+    tag = models.ManyToManyField(Tag)
+    publication = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.name
