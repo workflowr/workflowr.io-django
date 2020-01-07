8005253ca02011315485834aa5a5f8823904fe92
John Blischak <jdblischak@gmail.com>
2020-01-07 15:30:28 -0500

    Create generic views for Tag model.
    https://docs.djangoproject.com/en/3.0/intro/tutorial04/#use-generic-views-less-code-is-better

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'projects'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tags/', views.TagList.as_view(), name='tag_list'),
+    path('tag/<int:pk>', views.TagDetail.as_view(), name='tag_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Tag
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class TagList(generic.ListView):
+    model = Tag
+
+
+class TagDetail(generic.DetailView):
+    model = Tag
