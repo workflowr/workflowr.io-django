3174ea255b6742e152da33bd757c0c75f8b4b842
John Blischak <jdblischak@gmail.com>
2020-02-19 10:03:23 -0500

    Style tag list.

--- projects/views.py
+++ projects/views.py
@@ -44,6 +44,7 @@ class ProjectList(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['platforms'] = Platform.objects.all()
+        context['tags'] = Tag.objects.all()
         return context
 
 
