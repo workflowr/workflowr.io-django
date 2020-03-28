399fe2f43b4aa89a3ae0a293c98737a8a620a366
John Blischak <jdblischak@gmail.com>
2020-03-28 18:15:42 -0400

    Add 'workflowr.herokuapp.com' to ALLOWED_HOSTS.
    This is following the advice from the error:
    
    Invalid HTTP_HOST header: 'workflowr.herokuapp.com'. You may need to add
    'workflowr.herokuapp.com' to ALLOWED_HOSTS.
    
    This is pretty frustrating. The example Django app, which I deployed
    with no problem, had the same empty ALLOWED_HOSTS as I did.
    
    https://github.com/heroku/python-getting-started/blob/443a90c58be6881583cd7ef628e3869e3c30bb98/gettingstarted/settings.py#L30

--- main/settings.py
+++ main/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'p*bcjnbtbd!l(lfy16_xf=ouay558cxshl_n209=cxf441--xj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['workflowr.herokuapp.com']
 
 # Application definition
 
