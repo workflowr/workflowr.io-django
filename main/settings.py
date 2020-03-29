eb46dcc91876499b965ade579c00309b304c7c33
John Blischak <jdblischak@gmail.com>
2020-03-28 20:14:35 -0400

    Use whitenoise to serve static assets.
    As recommended in official docs, I put whitenoise immediately after the
    Django security middleware.
    
    http://whitenoise.evans.io/
    https://devcenter.heroku.com/articles/django-assets

--- main/settings.py
+++ main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +121,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
