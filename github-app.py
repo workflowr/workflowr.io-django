ac575187c05d021a68b7a91b7950fcf43e94cb93
John Blischak <jdblischak@gmail.com>
2020-05-12 12:01:16 -0400

    Try to authenticate with app via JSON Web Token (JWT).
    This failed miserably with a bad credentials error. I tried both
    with conda-forge PyGithub (1.43.6) and the latest release 1.51 with
    integrations support:
    
    pip install PyGithub['integrations']
    
    Frustratingly there doesn't seem to be a way to specify the extra
    integrations support in environment.yml. And it's not included in
    `pip freeze` or `conda env export`.
    
    Using PyGithub as a GitHub App is essentially undocumented. I've had to
    piece together clues from the Issues, Pull Requests, and tests. It says
    that that part of the API is supported:
    
    https://pygithub.readthedocs.io/en/latest/github_objects/Installation.html
    
    But I can't figure out how to authenticate. Passing the jwt token from
    my app to the main class didn't work unfortunately.
    
    At some point I'm just going to have to use pyjwt and requests directly.
    At least that way I can follow the GitHub API documentation instead of
    playing detective.

--- github-app.py
+++ github-app.py
@@ -8,3 +8,13 @@ dotenv.load_dotenv(verbose=True)
 GITHUB_PRIVATE_KEY = os.getenv("GITHUB_PRIVATE_KEY")
 GITHUB_APP_IDENTIFIER = os.getenv("GITHUB_APP_IDENTIFIER")
 GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET")
+
+app = github.GithubIntegration(
+  GITHUB_APP_IDENTIFIER,
+  GITHUB_PRIVATE_KEY,
+)
+jwt = app.create_jwt()
+gh = github.Github(jwt = jwt)
+repos = gh.get_repos()
+for repo in repos:
+    print(repo)
